891e9072b93c0afda2dc442b0bc4ebe92e596da4
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2023-03-08 20:20:37 -0300

    chore: add dotenv and config tables

---
+++ src/queries/tables.js
@@ -0,0 +1,50 @@
+const createTables = {
+  createUsers: () => {
+    return {
+      name: "create-users",
+      text: `
+        CREATE TABLE users (
+          id    SERIAL PRIMARY KEY,
+          name  TEXT NOT NULL,
+          email TEXT NOT NULL UNIQUE
+        )
+      `,
+    };
+  },
+  createCategories: () => {
+    return {
+      name: "create-categories",
+      text: `
+        CREATE TABLE categories (
+          id   SERIAL PRIMARY KEY,
+          name TEXT NOT NULL
+        )
+      `,
+    };
+  },
+  createFinances: () => {
+    return {
+      name: "create-finances",
+      text: `
+        CREATE TABLE finances (
+          id          SERIAL PRIMARY KEY,
+          user_id     INT,
+          category_id INT,
+          date        DATE,
+          title       TEXT,
+          value       NUMERIC,
+          CONSTRAINT fk_categories_finances
+            FOREIGN KEY (category_id)
+              REFERENCES categories(id)
+            ON DELETE SET NULL,
+          CONSTRAINT fk_users_finances
+            FOREIGN KEY (user_id)
+              REFERENCES users(id)
+            ON DELETE CASCADE
+        )
+      `,
+    };
+  },
+};
+
+module.exports = createTables;
